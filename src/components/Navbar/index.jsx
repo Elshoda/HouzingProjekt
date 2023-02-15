352f9214b856f7ecde47eea215ec7be7d0f3e1f0
Elshoda <amirxonelshoda@mail.ru>
2023-02-15 11:58:33 +0500

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
-import { Container, Section, Wrapper } from './style';
+import {   Navigate, Outlet } from 'react-router-dom';
+import navbar from '../../utils/navbar';
+import { Container, Link, Logo, Section, Wrapper } from './style';
 
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-      <Section>
-        <h1>Logo</h1>
+      <Section d='flex' c='pointer' onClick={()=>Navigate('/home')}>
+        <Logo/>
+        <h3>Logo</h3>
       </Section>
       <Section>
-        <NavLink>Home</NavLink>
-        <NavLink>Properties</NavLink>
+        {
+          navbar.map(({title,path},index)=>{
+            return(
+              <Link key={index} to={path}>{title}</Link>
+            )
+
+          })
+        }
+      </Section>
+      <Section>
+        <button>Login</button>
       </Section>
       </Wrapper>
       <Outlet/>
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,4 +1,6 @@
+import { NavLink } from 'react-router-dom';
 import styled, { css } from 'styled-components';
+import {ReactComponent as logoImg} from '../../assets/icon/logo.svg'
 
 const center =css`
     display: flex;
@@ -10,12 +12,31 @@ export const Container=styled.div`
     
 `
 export const Wrapper=styled.div`
+
     background:var(--colorPrimary) ;
     color: #fff;
     padding: var(--padding);
+    height: 64px;
+    font-size:16px ;
     ${center}
 `
 
 export const Section=styled.div`
-    
+    display: ${({d})=>d} !important;
+    align-items: center;
+    cursor:${({c})=>c} !important;
+`
+export const Logo=styled(logoImg)`
+    width:30px;
+    margin-right: 10px;
+
+    & path{
+        fill: #fff;
+        stroke: #fff;
+    }
+`
+export const Link=styled(NavLink)`
+    text-decoration: none;
+    color: #ffff;
+    padding: 0,30px;
 `
