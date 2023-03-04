2c45f916012da38bdbf68c394d712f00efd890f9
Elshoda <amirxonelshoda@mail.ru>
2023-03-04 21:31:46 +0500

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import {   Navigate, Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import navbar from '../../utils/navbar';
 import { Container, Link, Logo, Section, Wrapper } from './style';
 
 const Navbar = () => {
+  const navigate=useNavigate()
   return (
     <Container>
       <Wrapper>
-      <Section d='flex' c='pointer' onClick={()=>Navigate('/home')}>
-        <Logo/>
-        <h3>Logo</h3>
-      </Section>
-      <Section>
-        {
-          navbar.map(({title,path},index)=>{
-            return(
-              <Link key={index} to={path}>{title}</Link>
-            )
+        <Section d='flex' c='pointer' fw='900' onClick={() => navigate('/home')}>
+          <Logo />
+          <h3>Houzing</h3>
+        </Section>
+        <Section>
+          {
+            navbar.map(({ title, path,id }, index) => {
+              return (
+                <Link className={({isActive})=>isActive&&'active'} key={index} to={path}>{title}</Link>
+              )
 
-          })
-        }
-      </Section>
-      <Section>
-        <button>Login</button>
-      </Section>
+            })
+          }
+        </Section>
+        <Section>
+          <button>Login</button>
+        </Section>
       </Wrapper>
-      <Outlet/>
+      <Outlet />
     </Container>
   );
 }
